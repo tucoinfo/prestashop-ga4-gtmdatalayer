60f2af08452a840933dc8540beefb405e363f740
tucoinfo <leon@tucoinfo.fr>
2023-05-24 20:05:40 +0200

    Update gtmdatalayer.php

--- gtmdatalayer.php
+++ gtmdatalayer.php
@@ -9,7 +9,7 @@ class GtmDataLayer extends Module
 		$this->name = 'gtmdatalayer';
 		$this->table_name = 'gtm_data_layer';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.3';
+		$this->version = '1.4';
 		$this->author = 'petrovv77';
 		$this->need_instance = 0;
 		$this->ps_versions_compliancy = array('min' => '1.5', 'max' => _PS_VERSION_);
@@ -30,10 +30,10 @@ class GtmDataLayer extends Module
 	public function install()
 	{
 		if (!parent::install() ||
-			!$this->registerHook('paymentReturn'))
+			!$this->registerHook('orderConfirmation'))	// paymentReturn has objOrder but is locked to the single module that was used for payment, uses it's own confirmation tpl
 			return false;
 		if (!Db::getInstance()->Execute('
-			CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.{$this->table_name}.'` (
+			CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.$this->table_name.'` (
 				`id_data_layer` int(11) NOT NULL AUTO_INCREMENT,
 				`id_order` int(11) NOT NULL,
 				`sent` tinyint(1) DEFAULT NULL,
@@ -51,14 +51,14 @@ class GtmDataLayer extends Module
 		if (!parent::uninstall())
 			return false;
 
-		return Db::getInstance()->Execute('DROP TABLE IF EXISTS `'._DB_PREFIX_.{$this->table_name}.'`');
+		return Db::getInstance()->Execute('DROP TABLE IF EXISTS `'._DB_PREFIX_.$this->table_name.'`');
 	}
 
 
 	/**
 	* To track transactions
 	*/
-	public function hookPaymentReturn($params)
+	public function hookOrderConfirmation($params)
 	{
 		if (version_compare(_PS_VERSION_, '1.7', '<')) {
     			$order = $params['objOrder'];
@@ -68,31 +68,31 @@ class GtmDataLayer extends Module
 		
 		if (Validate::isLoadedObject($order))
 		{
-			$gtm_order_sent = Db::getInstance()->getValue('SELECT sent FROM `'._DB_PREFIX_.{$this->table_name}.'` WHERE id_order = '.(int)$order->id);
+			$gtm_order_sent = Db::getInstance()->getValue('SELECT sent FROM `'._DB_PREFIX_.$this->table_name.'` WHERE id_order = '.(int)$order->id);
 			if ($gtm_order_sent === false)
 			{
 				$order_products = array();
 				$cart = new Cart($order->id_cart);
 				foreach ($cart->getProducts() as $order_product) {
 					// https://developers.google.com/analytics/devguides/collection/ga4/ecommerce?client_type=gtm#make_a_purchase_or_issue_a_refund
-					$item_name = str_replace("'", '"', $order_product['name']);
-					$item_id = $order_product['id_product'];
+					$item_name = $order_product['name'];
+					$item_id = (int)$order_product['id_product'];
 					$price = number_format($order_product['price_wt'], 2, '.', '');
-					$quantity = $order_product['cart_quantity'];
+					$quantity = (int)$order_product['cart_quantity'];
 					$order_products[] = ['item_name' => $item_name, 'item_id' => $item_id, 'price' => $price, 'quantity' => $quantity];
 				}
 				
 				$products_json = json_encode($order_products);
                
-				Db::getInstance()->Execute('INSERT INTO `'._DB_PREFIX_.{$this->table_name}.'` (id_order, sent, date_add) VALUES ('.(int)$order->id.', 1, NOW())');
+				Db::getInstance()->Execute('INSERT INTO `'._DB_PREFIX_.$this->table_name.'` (id_order, sent, date_add) VALUES ('.(int)$order->id.', 1, NOW())');
 
 				$dl = new stdClass();
 				$dl->transaction_id = $order->id;
 				$dl->value = number_format($order->total_paid, 2, '.', '');
 				$dl->tax = ($order->total_paid_tax_incl - $order->total_paid_tax_excl);
 				$dl->shipping = number_format($order->total_shipping, 2);
-				$dl->currency = 'EUR';
-				$dl->items = $products_json;
+				$dl->currency = $params['currencyObj']->iso_code;
+				$dl->items = (int)$products_json;
 				
 				Media::addJsDef(array('dl' => $dl));
           			$this->context->controller->addJS($this->_path.'views/js/gtmdatalayer.js');

40d7db6e64611fe28b64454549d265783962500e
tucoinfo <leon@tucoinfo.fr>
2023-06-02 17:58:27 +0200

    Update gtmdatalayer.js

--- views/js/gtmdatalayer.js
+++ views/js/gtmdatalayer.js
@@ -1,13 +1,13 @@
 window.dataLayer = window.dataLayer || [];
 dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
 dataLayer.push({
-	'event': 'purchase',
-	'ecommerce': {
-		'transaction_id': dl.transaction_id,
-		'value': dl.value,
-		'tax': dl.tax,
-		'shipping': dl.shipping,
-		'currency': dl.currency,
-		'items': JSON.parse(dl.items),
+	event: 'purchase',
+	ecommerce: {
+		transaction_id: String(dl.transaction_id),
+		value: Number(dl.value),
+		tax: Number(dl.tax),
+		shipping: Number(dl.shipping),
+		currency: String(dl.currency),
+		items: JSON.parse(dl.items),
 	}
 });
